e215b49639b206c07390bc19faec95ddd3dd7fa3
chitreswari dash <chitreswaridash@gmail.com>
2026-06-21 13:11:39 +0000

    new commit

--- javascript/memory.js
+++ javascript/memory.js
@@ -21,4 +21,22 @@ const name=new String("chitra")
 //we can access value of any position in the string using index number
 //console.log(name.__proto__) this will give the prototype of the string object
 // all these methods can be see in the browser console 
+/*
+to slice a string we can do it in two ways
+1. using slice method
+2. using substring method
+const str="chitra"
+console.log(str.slice(0,3)) this will give "chi"
+console.log(str.substring(0,3)) this will give "chi"
+trim method is used to remove white spaces from the string
+console.log(str.trim())
 
+in an url we can replace the spaces with %20 using replace method
+const url="https://www.google.com/search?q=javascript memory"
+console.log(url.replace(" ","%20"))
+url.includes() method is used to check if a string contains a substring or not
+console.log(url.includes("javascript")) this will give true
+url.split() method is used to split a string into an array of substrings
+console.log(url.split(" ")) this will give ["https://www.google.com/search?q=javascript", "memory"]
+
+ */
